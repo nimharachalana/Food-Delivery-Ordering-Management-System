82582a59859cccfd4218db87cf2bfd750c877659
Nimhara Chalana <162380399+nimharachalana@users.noreply.github.com>
2026-01-09 19:04:58 +0530

    Add and Create place order feature and stripe payment integration.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
 import LoginPopup from './components/LoginPopup/LoginPopup';
+import Verify from './pages/Verify/verify';
 
 const App = () => {
 
@@ -22,6 +23,7 @@ const App = () => {
           <Route path='/' element={<Home /> } />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/Verify' element={<Verify />} />
         </Routes>
       </div>
       <Footer />
